27722da71fb8de1366f7f77ec6f37fc48f77054b
Sakshi Priya <priyasakshi8989@gmail.com>
2025-02-26 16:46:08 +0530

    footer created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     return(
         <Router>
             <Navbar />
+            <Footer />
         </Router>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../styles.css"
+
+const Footer = () => {
+    return(
+        <footer className="footer">
+            <p> &copy; 2025 India Tourism. All Rights Reserved. </p>
+        </footer>
+    );
+};
+
+export default Footer;
