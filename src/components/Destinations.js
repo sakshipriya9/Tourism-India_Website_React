314fe39a3f17f5c40103a5fc5865b0c0256e5c9c
Sakshi Priya <priyasakshi8989@gmail.com>
2025-02-27 21:51:34 +0530

    Added images in destination pages

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -3,14 +3,58 @@ import "../styles.css";
 const Destinations = () => {
     return(
         <div className="destinations">
-            <h2> Top Destinations in India </h2>
-            <ul>
-                <li> Taj Mahal, Agra</li>
-                <li> Jaipur, Rajasthan</li>
-                <li> Goa Beaches </li>
-                <li> Kashmir </li>
-                <li> Backwaters of Kerala </li>
-            </ul>
+            <h2> Top Destinations in India... </h2>
+            <div className="destination-grid">
+                <div className="destination-card">
+                    <img src="/Images/img 1.jpeg" alt="Taj Mahal, Agra" />
+                    <p> Taj Mahal, Agra </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 2.jpg" alt="Kerela, Back Waters" />
+                    <p> Kerela Back Waters, Kerela </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 3.webp" alt=" Hawa Mahal, Jaipur, Rajasthan" />
+                    <p> Hawa Mahal, Jaipur, Rajasthan </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 4.webp" alt="Lakshadweep" />
+                    <p> Lakshadweep Beaches, Lakshadweep </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 5.jpg" alt="Amrit Udyan, Rashtrapati Bhawan, Delhi" />
+                    <p> Amrit Udyan, Rashtrapati Bhawan, Delhi</p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 6.jpg" alt="Shimla" />
+                    <p>  Shimla, Himachal Pradesh </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 7.jpeg" alt="Banaras Ghat" />
+                    <p> Banaras Ghat, Banaras </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 8.jpg" alt="Red Fort, Delhi" />
+                    <p> Red Fort, Delhi </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 9.jpeg" alt="Golden Temple, Amritsar" />
+                    <p> Golden Temple, Amritsar, Punjab </p>
+                </div>
+
+                <div className="destination-card">
+                    <img src="/Images/img 10.jpg" alt="Tea Garden, Darjeling" />
+                    <p> Tea Garden, Darjeling </p>
+                </div>
+            </div>
         </div>
     );
 };
